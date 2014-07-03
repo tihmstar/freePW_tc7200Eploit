e5f9fe1662265a5b85a82551eb67f9a02498dfc5
tihmstar <tihmstar@gmail.com>
2014-07-03 15:36:22 +0200

    Initial Commit

---
+++ UMSucks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UMSucks
+//
+//  Created by tihm on 03.07.14.
+//  Copyright (c) 2014 tihmstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UMSucks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UMSucks
+//
+//  Created by tihm on 03.07.14.
+//  Copyright (c) 2014 tihmstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
